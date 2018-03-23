990c95a706d82db25716e6a05e6532f657dd3a78
Can Bulut Bayburt <cbbayburt@suse.com>
2018-03-23 15:48:17 +0100

    Add 'undo' and no-arg options to recreate script

--- recreate.sh
+++ recreate.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-terraform taint -module $1 libvirt_domain.domain && \
-terraform taint -module $1 libvirt_volume.main_disk
+if [ "$1" = "-u" -o "$1" = "--undo" ]
+then
+    CMD=untaint
+    RESOURCES=$2
+else
+    CMD=taint
+    RESOURCES=$1
+fi
+
+if [ -z $RESOURCES ]
+then
+# Only test:    RESOURCES=$(terraform state list | grep "module\..*\-test\." | cut -d. -f 2,4 | sort -u)
+    RESOURCES=$(terraform state list | grep "^module\..*\.libvirt_domain.domain$" | cut -d. -f2,4)
+fi
+
+for RES in $RESOURCES
+do
+    terraform $CMD -module $RES libvirt_domain.domain && \
+    terraform $CMD -module $RES libvirt_volume.main_disk
+done
+
+
+# TODO: Better argument parsing and recreate all
+# (when no resource names given) option
+# recreate [-u|--undo] [-f|--filter FILTER] [resource [...]]
