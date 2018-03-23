aafdef4028c5abf5574520342bd9e08c8fb5d241
Can Bulut Bayburt <cbbayburt@suse.com>
2018-03-23 15:50:38 +0100

    Add launch-testsuite script

---
+++ launch-testsuite.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# TODO: Add state file path option
+# TODO: Add option: --core-only
+
+if [ -z "$(terraform show | grep module\\.)" ]
+then
+    terraform init
+else
+    ./recreate.sh
+fi
+
+terraform get
+terraform apply
+
+CTRL_NAME=$(terraform state show $(terraform state list | grep controller.libvirt_domain) |\
+    grep ^name | sed -r s/\\s+//g | cut -d= -f2)
+CTRL_FQDN=$CTRL_NAME.tf.local
+
+ssh-keygen -R $CTRL_FQDN
+ssh-keyscan $CTRL_FQDN >> ~/.ssh/known_hosts
+
+ssh root@$CTRL_FQDN screen -d -m run-testsuite
