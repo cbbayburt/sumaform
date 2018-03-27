3f7272ce5da47d1f8672eedae46f43c64b968c03
Can Bulut Bayburt <cbbayburt@suse.com>
2018-03-27 14:01:47 +0200

    Add development environment options for launch-testsuite script

--- launch-testsuite.sh
+++ launch-testsuite.sh
@@ -6,7 +6,7 @@
 # Author: Can Bayburt <cbbayburt@suse.com>
 #
 # Usage:
-#   launch-testsuite [-c|--core-only] [-s|--state <tfstate file>]
+#   launch-testsuite [-c|--core-only] [-s|--state <tfstate file>] [--spacewalk-dir <spacewalk directory>]
 #
 # TODO:
 #   - Break if a command fails
@@ -21,6 +21,11 @@ do
             ;;
         -s|--state)
             STATEPATH="$2"
+            shift
+            shift
+            ;;
+        --spacewalk-dir)
+            SPACEWALKDIR="$2"
             shift
             shift
             ;;
@@ -50,6 +55,25 @@ fi
 terraform get
 terraform apply $TFSTATEARG
 
+if [ -n "$SPACEWALKDIR" ]
+then
+    SRV_NAME=$(terraform state show $TFSTATEARG $(terraform state list $TFSTATEARG | grep suse_manager.libvirt_domain) |\
+        grep ^name | sed -r s/\\s+//g | cut -d= -f2)
+    SRV_FQDN=$SRV_NAME.tf.local
+    ssh-keygen -R $SRV_FQDN
+    ssh-keyscan $SRV_FQDN >> ~/.ssh/known_hosts
+
+    # Deploy java
+    CURDIR=$(pwd)
+    cd $SPACEWALKDIR/java
+    ant resolve-ivy
+    ant -f manager-build.xml -Ddeploy.host=$SRV_FQDN refresh-branding-jar deploy
+
+    sleep 120
+
+    cd $CURDIR
+fi
+
 CTRL_NAME=$(terraform state show $TFSTATEARG $(terraform state list $TFSTATEARG | grep controller.libvirt_domain) |\
     grep ^name | sed -r s/\\s+//g | cut -d= -f2)
 CTRL_FQDN=$CTRL_NAME.tf.local
@@ -62,7 +86,9 @@ then
     ssh root@$CTRL_FQDN sed -i /core_.*\.feature/\!d spacewalk/testsuite/run_sets/testsuite.yaml
 fi
 
-ssh root@$CTRL_FQDN screen -d -m run-testsuite
+ssh -t root@$CTRL_FQDN run-testsuite
 
-echo "Testsuite run is started in a screen session."
-echo "Run 'ssh -t root@$CTRL_FQDN screen -r' to attach to it."
+## Alternatively, run in a screen session:
+# ssh root@$CTRL_FQDN screen -d -m run-testsuite
+# echo "Testsuite run is started in a screen session."
+# echo "Run 'ssh -t root@$CTRL_FQDN screen -r' to attach to it."
